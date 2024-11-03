9017e7c166e3c16e07343375836142aeb0ed4403
Anushree Mangalgatti <anushreemangalgatti68@gmail.com>
2024-11-03 15:18:06 +0530

    Add files via upload

---
+++ 0.py
@@ -0,0 +1,11 @@
+#First programming in python 
+print("All the best for all programers")
+
+#output: All the best for all programers
+
+
+#Assigning integer value to the variables
+a = 10
+b = 6
+print(a-b)
+#output: 
