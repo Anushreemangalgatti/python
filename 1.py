ffd2642604898351a0c5f2e36760a0d5302e2185
Anushree Mangalgatti <anushreemangalgatti68@gmail.com>
2024-11-03 15:23:51 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,16 @@
+#Finding a data type
+Name="Anushree"
+age=19
+is_student=False
+pai_value=3.142
+print(type(Name))             #output:<class 'str'>
+print(type(age))              #output:<class 'int'>
+print(type(is_student))       #output:<class 'bool'>
+print(type(pai_value))        #output:<class 'float'>
+
+
+#convert float value into int value and string into int value
+variable="2"
+hight=5.9
+print(int(hight))                #output: 5
+print(int(variable)+hight)       #output:7.9
