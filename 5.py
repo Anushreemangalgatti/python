85c5bf6e8f12a9629975e538675b0e75a635d77a
Anushree Mangalgatti <anushreemangalgatti68@gmail.com>
2024-11-03 15:27:57 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,20 @@
+#swapcase of string
+
+Name_1 ="anushree"
+Name_2 ="ANUSHREE"
+Name_3 ="AnUsHrEe"
+
+#converts string into uppercase
+print(Name_1.swapcase())
+
+#converts string intolowercase
+print(Name_2.swapcase())
+
+#converts string uppercaseto lowercase and vise versa
+print(Name_3.swapcase())
+
+
+#output
+#ANUSHREE
+#anushree
+#aNuShReE
