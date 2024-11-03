0e84d0831de60f192692ac67d879ee7f1c62a930
Anushree Mangalgatti <anushreemangalgatti68@gmail.com>
2024-11-03 16:13:59 +0530

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,25 @@
+Name=input("Enter your name: ")        #To take input from the user
+age=int(input("Enter your age: "))     #convert input into integer
+#output: Anushree
+#output: 19
+
+#convert output to the console
+Her_name="Anushree"
+Her_friend="Bhavya"
+print(Her_name+ " is more inteligent than "+Her_friend)
+#output: Anushree is more inteligent than Bhavya
+
+#convert output to the console by using formatted string
+Her_name: "Anushree"
+Her_friend: "Bhavya"
+print(f"{Her_name} is more inteligent than {Her_friend}")
+#output: Anushree is more inteligent than Bhavya
+
+#convert output to the console by taking input from the user
+name=input("Enter your name: ")
+PUC= input("Enter your college name: ")
+college=input("Enter the degree college name: ")
+perc=float(input("Enter the the percentage: "))
+print(f"My name is {name}. I completed my PUC in {PUC}. Corently i am studing in {college}. I got percentage in 2nd PUC is {perc}")
+#output: My name is Anushree . I completed my PUC in ICS mahesh PU college Dharwad. Corently i am studing in Jain college of engineering and technology hubli. I got percentage in 2nd PUC is 87.16
+
