eadc9c0f038b2ce18368641687c3cbd8ec64e6e3
Anushree Mangalgatti <anushreemangalgatti68@gmail.com>
2024-11-03 15:26:22 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,15 @@
+#The swaping of two variables
+
+x = 8
+y = 9
+
+#creat a tenprory variable and swaping the values
+temp = x
+x = y
+y = temp
+
+print('x = {}'.format(x))
+print('y = {}'.format(y))
+      
+
+#output: x = 9 and y = 8
