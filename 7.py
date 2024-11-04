0dbe7c20613ee6dba2d009b6aedf1bcfdf924d21
Anushree Mangalgatti <anushreemangalgatti68@gmail.com>
2024-11-04 20:52:24 +0530

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,16 @@
+#String Manipulation
+#Concatenation
+Mother_name="Mahadevi"
+Sername_name="Mangalgatti"
+Full_name=Mother_name+" "+Sername_name
+print(Full_name)
+
+#output: Mahadevi Mangalgatti
+
+#repitition
+Advice="don't make silly mistake"
+print(Advice*5)
+
+#output: don't make silly mistakedon't make silly mistakedon't make silly mistakedon't make silly mistakedon't make silly mistake
+
+
